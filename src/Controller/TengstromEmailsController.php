c33e7bf48788a90165790476d90fc760fdff3d7d
Kristofer Tengstrom <info@kristofer-tengstrom.se>
2023-08-11 13:45:48 +0200

    Fix linter issues

--- src/Controller/TengstromEmailsController.php
+++ src/Controller/TengstromEmailsController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Drupal\tengstrom_emails\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
--- src/Routing/RouteSubscriber.php
+++ src/Routing/RouteSubscriber.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Drupal\tengstrom_emails\Routing;
 
 use Drupal\Core\Routing\RouteSubscriberBase;
