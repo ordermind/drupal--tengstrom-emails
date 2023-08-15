80bf934c297a5d4cc2cded3786a93505da730a25
Kristofer Tengstrom <info@kristofer-tengstrom.se>
2023-08-15 14:00:50 +0200

    Use the css from the default theme and its parent theme in e-mails

--- src/Plugin/EmailAdjuster/InlineCssEmailAdjuster.php
+++ src/Plugin/EmailAdjuster/InlineCssEmailAdjuster.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace Drupal\tengstrom_emails\Plugin\EmailAdjuster;
 
+use Drupal\Core\Asset\AssetResolverInterface;
 use Drupal\Core\Asset\AttachedAssets;
+use Drupal\Core\Extension\ThemeHandlerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\symfony_mailer\EmailInterface;
 use Drupal\symfony_mailer\Plugin\EmailAdjuster\InlineCssEmailAdjuster as OverriddenAdjuster;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Defines the Inline CSS Email Adjuster.
@@ -18,7 +22,33 @@ use Drupal\symfony_mailer\Plugin\EmailAdjuster\InlineCssEmailAdjuster as Overrid
  *   weight = 900,
  * )
  */
-class InlineCssEmailAdjuster extends OverriddenAdjuster {
+class InlineCssEmailAdjuster extends OverriddenAdjuster implements ContainerFactoryPluginInterface {
+  protected ThemeHandlerInterface $themeHandler;
+
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    AssetResolverInterface $assetResolver,
+    ThemeHandlerInterface $themeHandler
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $assetResolver);
+
+    $this->themeHandler = $themeHandler;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('asset.resolver'),
+      $container->get('theme_handler')
+    );
+  }
 
   /**
    * Adjustments:
@@ -27,9 +57,14 @@ class InlineCssEmailAdjuster extends OverriddenAdjuster {
    * since that does not work with the scss compiler.
    */
   public function postRender(EmailInterface $email) {
+    $siteTheme = $this->themeHandler->getDefault();
+    $parentTheme = $this->themeHandler->getTheme($siteTheme)->base_theme;
+
     // Inline CSS. Request optimization so that the CssOptimizer performs
     // essential processing such as @import.
-    $assets = (new AttachedAssets())->setLibraries(['tengstrom_2022/emails']);
+    $assets = (new AttachedAssets())->setLibraries(
+      ["{$parentTheme}/emails", "{$siteTheme}/emails"]
+    );
     $css = '';
     foreach ($this->assetResolver->getCssAssets($assets, FALSE) as $file) {
       $css .= file_get_contents($file['data']);
